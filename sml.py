c8981814ec6f54f213df5e7833e6dde67cec86e9
Shamil Habeeb <shamilhabeeb5@gmail.com>
2022-12-17 20:08:17 +0530

    Update sml.py

--- sml.py
+++ sml.py
@@ -2,9 +2,9 @@ import discord
 import json
 import requests
 from discord.ext import commands
-from index import BOT_TOKEN as TOKEN
-
-demo = commands.Bot(command_prefix='!','/','#') 
+from conf import BOT_TOKEN as TOKEN
+from conf import CMD_PRE as CMD_PREFIX
+demo = commands.Bot(command_prefix=CMD_PREFIX) 
 
 @demo.command()
 async def test(clienT, arg):  # test fn
