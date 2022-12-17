69272b389d03b4fabff99f298e401818a508fe7b
Shamil Habeeb <shamilhabeeb5@gmail.com>
2022-12-18 04:55:44 +0530

    Allara chillara

--- sml.py
+++ sml.py
@@ -46,30 +46,30 @@ async def print(clienT, *cmd):
       
 @demo.command(name='img')
 async def img(clienT, argu): 
-    response = requests.get('https://some-random-api.ml/img/{}'.format(argu)) 
-    jsoon = json.loads(response.text)  
+    txt = requests.get('https://some-random-api.ml/img/{}'.format(argu)) 
+    jsoon = json.loads(txt.text)  
 
-    embed = discord.Embed(color=0x72BAB6, title='Random image with {}'.format(arg_im))  
+    embed = discord.Embed(color=0x72BAB6, title='Random image with {}'.format(argu))  
     embed.set_image(url=jsoon['link'])  #telegraph link
     await clienT.send(embed=embed)  
     
 @demo.command(name='add') # "!add 1 2 " output = 3
-async def addition(clienT, a,b):
+async def add(clienT, a,b):
     summ = int(a)+int(b)
     await clienT.send(summ)
     
 @demo.command(name='minus') # "!minus 2 1" output = 1
-async def substract(clienT, a,b):
+async def minus(clienT, a,b):
     sub = int(a)-int(b)
     await clienT.send(sub)
 
 @demo.command(name='multi') # "!multi 1 2" output = 2
-async def multiplication(clienT, a,b):
+async def multi(clienT, a,b):
     asterisk = int(a)*int(b)
     await clienT.send(asterisk)
     
 @demo.command(name='div') # "!div 2 1" output = 2
-async def division(clienT, a,b):
+async def div(clienT, a,b):
     floor = int(a)/int(b)
     await clienT.send(floor)
 
