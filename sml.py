8c6ad7d0384e6d763b4542a5f0bd7f85db9121fd
Shamil Habeeb <shamilhabeeb5@gmail.com>
2022-12-17 20:22:57 +0530

    Mathematical Fns

--- sml.py
+++ sml.py
@@ -23,7 +23,26 @@ async def img(clienT, argu):
     embed = discord.Embed(color=0x72BAB6, title='Random image with {}'.format(arg_im))  
     embed.set_image(url=jsoon['link'])  #telegraph link
     await clienT.send(embed=embed)  
+    
+@demo.command(name='add') # "!add 1 2 " output = 3
+async def addition(clienT, a,b):
+    summ = int(a)+int(b)
+    await clienT.send(summ)
+    
+@demo.command(name='minus') # "!minus 2 1" output = 1
+async def substract(clienT, a,b):
+    sub = int(a)-int(b)
+    await clienT.send(sub)
 
+@demo.command(name='multi') # "!multi 1 2" output = 2
+async def multiplication(clienT, a,b):
+    asterisk = int(a)*int(b)
+    await clienT.send(asterisk)
+    
+@demo.command(name='div') # "!div 2 1" output = 2
+async def division(clienT, a,b):
+    floor = int(a)/int(b)
+    await clienT.send(floor)
 
 demo.run(TOKEN)  # launching 😌
     
